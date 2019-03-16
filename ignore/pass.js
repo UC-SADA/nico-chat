7ad491098e6b399cbf74da89d92bcd9b9e5661fe
sadamitsu yushi <uc.sadamitsu@gmail.com>
2019-03-17 00:53:06 +0900

    DB_test

--- ignore/pass.js
+++ ignore/pass.js
@@ -1,8 +1,8 @@
 //　DataBase アクセス用のID　と　Pass　を入力
 module.exports ={
-  database: 'test0',
-  user: 'test_u',
-  password: 'test',
-  host: "localhost",
+  database: 'test_d2',
+  user: 'test_u2',
+  password: 'test_p2',
+  host: "localhost_2",
   port:  5432
 }
